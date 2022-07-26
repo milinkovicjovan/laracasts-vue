25ceaeb49919d801320ec4726221885efa4426ae
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-07-26 17:05:15 +0200

    laracasts.vue

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,17 +1,14 @@
 import AssignmentList from './AssignmentList.js';
+import AssignmentCreate from './AssignmentCreate.js';
+
 export default {
-  components: { AssignmentList },
+  components: { AssignmentList, AssignmentCreate },
   template: ` 
   <section class="space-y-6">
    <assignment-list :assignments="filters.inProgress" title="In progress"></assignment-list>
    <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
 
-   <form @submit.prevent="add">
-    <div class="border border-gray-600 text-black">
-    <input v-model="newAssignment" placeholder="New assignment..." class="p-2" />
-    <button type="submit" class="bg-white p-2 border-l">Add</button>
-    </div>
-   </form>
+   <assignment-create @add="add"></assignment-create>
   </section>
   `,
   data() {
@@ -21,9 +18,9 @@ export default {
         { name: 'Read Chapter 4', complete: false, id: 2 },
         { name: 'Turn in Homework', complete: false, id: 3 },
       ],
-      newAssignment: '',
     };
   },
+
   computed: {
     filters() {
       return {
@@ -34,14 +31,14 @@ export default {
       };
     },
   },
+
   methods: {
-    add() {
+    add(name) {
       this.assignments.push({
-        name: this.newAssignment,
+        name: name,
         completed: false,
         id: this.assignments.length + 1,
       });
-      this.newAssignment = '';
     },
   },
 };
