1dbfda47746bebe096810e69888c2863f1c9dae3
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-10-14 20:26:57 +0200

    deeperLookAtV*model

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,5 +1,5 @@
-import Assignment from './Assignment.js';
-import AssignmentTags from './AssignmentTags.js';
+import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
   components: { Assignment, AssignmentTags },
@@ -8,9 +8,10 @@ export default {
     <h2 class="font-bold mb-2"> {{ title }}
     <span>({{ assignments.length }})</span>
     </h2>
-     <assignment-tags :initial-tags="assignments.map(a => a.tag)"
-     :current-tag="currentTag"
-     @change="currentTag = $event"  />
+     <assignment-tags 
+     v-model:currentTag="currentTag"
+     :initial-tags="assignments.map(a => a.tag)"
+     />
       <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
       <assignment v-for="assignment in filteredAssignments" :key="assignment.id"
       :assignment="assignment"></assignment>
@@ -23,12 +24,12 @@ export default {
   },
   data() {
     return {
-      currentTag: 'all',
+      currentTag: "all",
     };
   },
   computed: {
     filteredAssignments() {
-      if (this.currentTag === 'all') {
+      if (this.currentTag === "all") {
         return this.assignments;
       }
       return this.assignments.filter((a) => a.tag === this.currentTag);
